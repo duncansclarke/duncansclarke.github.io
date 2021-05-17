cca464f2226a96696eb8d8430571c899d1e980e1
duncansclarke <duncansclarke@gmail.com>
2021-05-17 15:29:13 -0400

    adjusted language proficiencies

--- portfolio-script.js
+++ portfolio-script.js
@@ -3,12 +3,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   move("python", 90);
   move("java", 90);
+  move("git", 90);
   move("html", 90);
   move("css", 80);
   move("js", 65);
-  move("react", 25);
+  move("react", 50);
   move("sql", 65);
-  move("git", 25);
   move("php", 25);
   move("tensorflow", 30);  
   move("pytorch", 40);
